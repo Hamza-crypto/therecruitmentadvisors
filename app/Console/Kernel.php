5ebdaf469083fa5af1e603294f1a05e9491d792e
Hamza <6793siddique@gmail.com>
2024-03-26 12:01:35 +0500

    Run Command Daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('rapidapi:fetch-jobs')->everyTwoMinutes();
+        $schedule->command('rapidapi:fetch-jobs')->daily();
     }
 
     /**
